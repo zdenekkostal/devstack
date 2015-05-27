505b437a605ca93828a3a260bb09c47675e5471b
Zdenek Kostal <zdenek.kostal@gooddata.com>
2015-05-27 23:20:31 +0200

    Extract styles during build

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -3,6 +3,7 @@ var path = require('path');
 var webpack = require('webpack');
 var getWebpackConfig = require('./webpack.config.js');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var uglifyOptions = {
     mangle: true,
@@ -27,7 +28,17 @@ var buildConfig = _.assign(getWebpackConfig(), {
     }
 });
 
+buildConfig.module.loaders.filter(function(definition) {
+    return definition.loader.indexOf('!css') !== -1;
+}).forEach(function(loaderDefinition) {
+    var loader = loaderDefinition.loader.replace('style!', '');
+
+    loaderDefinition.loader = ExtractTextPlugin.extract('style', loader);
+});
+
 buildConfig.plugins = buildConfig.plugins.concat(
+    new ExtractTextPlugin('[name].[hash].css'),
+
     new webpack.DefinePlugin({
         'process.env': {
             // This has effect on the react lib size
